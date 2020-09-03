92734b1bc282d9946e70542f42728a1593df9938
Oleksandr Pushkar <64772614+tauruskin@users.noreply.github.com>
2020-09-03 18:52:19 +0300

    Updates

---
+++ precache-manifest.255e6a2bc6abc3a4d6f2aba56556da3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b338e1cf2c98cd0e0347fa5b840e4763",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "20e6448e68f1b501ec0b",
+    "url": "/goit-react-hw-01-components/static/css/main.2274fb63.chunk.css"
+  },
+  {
+    "revision": "ec4401d773e098f1f986",
+    "url": "/goit-react-hw-01-components/static/js/2.36626dab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.36626dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e6448e68f1b501ec0b",
+    "url": "/goit-react-hw-01-components/static/js/main.3fdc70df.chunk.js"
+  },
+  {
+    "revision": "7181ea73fa66db6b9cd4",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.46575509.js"
+  }
+]);
